4d219cd58a70eb85ba8eeafc1bbed8b8dd2910ae
Sinkda <sinkda@gmail.com>
2022-04-26 17:37:55 -0400

    Add game logic for guessing

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -9,15 +9,16 @@ class Game extends React.Component
 {
     constructor(props) {
         super(props);
+
+        this.handleGuess = this.handleGuess.bind(this);
+
+        this.maxGuesses = 8;
+
         this.state = {
             pokemon: [],
-            rows: []
+            rows: [],
+            currentGuess: 1
         };      
-    }
-
-    renderRows()
-    {
-        return (<Row layout="cccud" />);
     }
 
     componentDidMount() {
@@ -29,7 +30,130 @@ class Game extends React.Component
         this.setState({
             pokemon: [name, pick]
         });
+
     }
+
+    handleGuess(guess)
+    {
+        this.setState({
+            currentGuess: this.state.currentGuess + 1
+        })
+
+        // handle win condition
+        if(guess == this.state.pokemon[0])
+        {
+            let rows = this.state.rows;
+            rows.push(<Row layout='ccccc' key={this.state.currentGuess} header={guess} />)
+            this.setState({
+                rows: rows
+            });
+
+            this.handleWinCondition();
+            return;
+        }
+
+        // populate the new row.
+        let pokemon = pokedex[guess];
+        let layoutString = this.calculateGuessLayout(pokemon);
+
+        console.log(pokemon);
+
+        let rows = this.state.rows;
+        rows.push(<Row layout={layoutString} key={this.state.currentGuess} header={guess} />)
+        this.setState({
+            rows: rows
+        });
+
+        // see if that was our last guess
+        if(this.state.currentGuess == this.maxGuesses)
+            this.handleLoseCondition();
+
+    }
+
+    calculateGuessLayout(guess)
+    {
+        let layoutString = '';
+        
+        layoutString += this.compareGeneration(guess[0]);
+        layoutString += this.compareTypes(guess[1], guess[2]);
+        layoutString += this.compareHeight(guess[3]);
+        layoutString += this.compareWeight(guess[4]);
+
+        return layoutString;
+    }
+
+    compareGeneration(guess)
+    {
+        let gen = this.state.pokemon[1][0];
+
+        if(gen == guess)
+            return 'c';
+        else if(gen > guess)
+            return 'u';
+        else
+            return 'd';
+    }
+
+    compareTypes(type1, type2)
+    {
+        let pokemonType1 = this.state.pokemon[1][1];
+        let pokemonType2 = this.state.pokemon[1][2];
+
+        let layoutString = '';
+
+        // find for type1
+        if(type1 == pokemonType1)
+            layoutString += 'c';
+        else if(type1 == pokemonType2)
+            layoutString += 'p';
+        else
+            layoutString += 'w';
+
+        // find for type2
+        if(type2 == pokemonType2)
+            layoutString += 'c';
+        else if(type2 == pokemonType1)
+            layoutString += 'p';
+        else
+            layoutString += 'w';
+
+        return layoutString;
+    }
+
+    compareHeight(height)
+    {
+        let pokemonHeight = this.state.pokemon[1][3];
+
+        if(pokemonHeight == height)
+            return 'c';
+        else if(pokemonHeight > height)
+            return 'u';
+        else;
+            return 'd';
+    }
+
+    compareWeight(weight)
+    {
+        let pokemonWeight = this.state.pokemon[1][4];
+
+        if(pokemonWeight == weight)
+            return 'c';
+        else if(pokemonWeight > weight)
+            return 'u';
+        else;
+            return 'd';
+    }
+
+    handleWinCondition()
+    {
+        console.log('YOU WIN!');
+    }
+
+    handleLoseCondition()
+    {
+        console.log('You Lost :(');
+    }
+
 
     render() {
 
@@ -44,11 +168,11 @@ class Game extends React.Component
                 </div>
                 
                 <div>
-                    { this.renderRows() }
+                    { this.state.rows }
                 </div>
 
                 <div>
-                    <Guess />
+                    <Guess handleGuess={this.handleGuess} />
                 </div>
             </div>
             );
--- src/components/Guess.jsx
+++ src/components/Guess.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import reactStringReplace from 'react-string-replace';
 import pokedex from '../assets/pokedex.json';
 
 
@@ -9,18 +10,26 @@ class Guess extends React.Component
         super(props);
 
         this.handleKeyPress = this.handleKeyPress.bind(this);
+        this.handleAutocompleteClick = this.handleAutocompleteClick.bind(this);
+        this.handleGuess = this.handleGuess.bind(this);
+        this.transitionGuessError = this.transitionGuessError.bind(this);
 
-        this.state = {
-            suggestions: null
+        this.inputRef = React.createRef();
+
+        this.defaultState = {
+            suggestions: null,
+            autocompleteState: 'hidden',
+            outlineErrorState: 'outline-none'
         };
+
+        this.state = this.defaultState;
     }
 
-    searchPokemonByName(name)
+    searchPokemonByName(search)
     {
         let searchLimit = 5;
 
-        let keys = Object.keys(pokedex);
-        let found = keys.filter(element => element.toLowerCase().startsWith(name.toLowerCase()));
+        let found = Object.keys(pokedex).filter(element => element.toLowerCase().includes(search.toLowerCase()));
 
         let output = found.slice(0, searchLimit);
 
@@ -29,34 +38,100 @@ class Guess extends React.Component
 
     populateSearch(found, criteria)
     {   
-        // const suggestions = found.map((value, index) => (<div key={index}>{
-            
-        //     // TODO:  Fix with a JSX Appropriate string replace
-        //     value.replace(
-        //         criteria,  
-        //         '<span className="font-semibold">{criteria}</span>'
-        //     )
+        const suggestions = found.map((value, index) => {
+             
+            return (
+            <div key={index} className="flex flex-col border-b-2 border-b-slate-800 hover:bg-slate-200 hover:cursor-pointer overflow-y"
+                    onClick={() => this.handleAutocompleteClick(value)}>
+                <div>
+                    {reactStringReplace(value, criteria, (match, i) => (<span className="font-bold" key={i}>{match}</span>))}
+                </div>
+                <div className="text-sm grid grid-cols-3 mt-1">
+                    <div><span className="font-semibold">Gen:</span> {pokedex[value][0]}</div>
+                    <div><span className="font-semibold">Type 1:</span> {pokedex[value][1]}</div>
+                    <div><span className="font-semibold">Type 2:</span> {(pokedex[value][2] !== '') ? pokedex[value][2] : 'None'}</div>
+                    <div><span className="font-semibold">Height:</span> {pokedex[value][3]}</div>
+                    <div><span className="font-semibold">Weight:</span> {pokedex[value][4]}</div>
+                </div>
+            </div>
+            );
+        });
 
-        // }</div>)) ;
+        this.setState({
+            suggestions: suggestions
+        });
+    }
 
-        // this.setState({
-        //     suggestions: suggestions
-        // });
+    handleAutocompleteClick(name)
+    {
+        this.inputRef.current.value = name;
+
+        this.setState({
+            autocompleteState: 'hidden'
+        });
     }
 
     handleKeyPress(e)
     {
-        let found = this.searchPokemonByName(e.target.value);
-        this.populateSearch(found, e.target.value);
+        let search = '';
+
+        // Catch for non-alphabetical keys (like Backspace, Delete, etc.)
+        if(e.keyCode >= 65 && e.keyCode <= 90)
+            search = e.target.value + e.key;
+        else
+            search = e.target.value;      
+
+        if(search === '')
+            this.setState({
+                autocompleteState: 'hidden'
+            });
+        else
+        {
+            let found = this.searchPokemonByName(search);
+            this.populateSearch(found, search);
+    
+            this.setState({
+                autocompleteState: 'flex'
+            });
+        }
+    }
+
+    handleGuess()
+    {
+        if(!pokedex.hasOwnProperty(this.inputRef.current.value))
+        {
+            this.setState({
+                outlineErrorState: 'outline outline-red-600'
+            });
+
+            setTimeout(this.transitionGuessError, 3000);
+            return;
+        }
+
+        this.props.handleGuess(this.inputRef.current.value);
+
+        this.setState(this.defaultState);
+        this.inputRef.current.value = '';
+    }
+
+    transitionGuessError()
+    {
+        this.setState({
+            outlineErrorState: 'outline-none'
+        });
     }
 
     render() {
         return (
             <div className="mt-6 relative">
                 <label htmlFor="guess" className="text-blue-200 text-lg">Guess a Pokemon</label>
-                <input type="text" onKeyDown={this.handleKeyPress} className="w-full p-2 mt-2 outline-none hover:outline-1 hover:outline-blue-200" name="guess" />
+                <div className="flex flex-row items-center w-full h-full mt-2">
+                    <input type="text" onKeyDown={this.handleKeyPress} className={`p-2 ${this.state.outlineErrorState} outline-2 hover:outline-blue-200 transition duration-200 ease-in-out`} 
+                           name="guess" ref={this.inputRef} placeholder="Who's That Pokemon?" autoComplete="off"/>
+                    <button className='shadow-md bg-blue-400 hover:bg-blue-600 text-white rounded-r-xl p-1 h-11 transition-all duration-200 ease-in-out' onClick={this.handleGuess}>Guess!</button>
+                </div>
 
-                <div className="absolute left-0 right-0 top-20 bg-white border-slate-900 flex flex-col">
+                <div className={`absolute left-0 right-0 top-20 bg-white border-slate-900 flex-col ${this.state.autocompleteState} rounded`} ref={this.autocompleteRef}>
                     { this.state.suggestions }
                 </div>
             </div>
