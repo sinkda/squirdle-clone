40382a1289845b79d6a0679e3995ce8c4c678aaa
Sinkda <sinkda@gmail.com>
2022-04-27 09:19:46 -0400

    Added default footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import Game from './components/Game';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -7,6 +8,8 @@ function App() {
       <Header />
 
       <Game />
+
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Footer extends React.Component
+{
+    render() {
+        return (
+            <div className="fixed bottom-0 left-0 right-0 h-20 bg-gray-900 text-white">
+                <div className="flex flex-col items-center mt-3 text-sm">
+                    Text
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Footer;
