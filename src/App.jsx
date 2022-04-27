7c9364dc2f09b816db2f019259e89ca80b91a95f
Sinkda <sinkda@gmail.com>
2022-04-27 09:19:31 -0400

    Fixed layout issues

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Game from './components/Game';
 
 function App() {
   return (
-    <div className="h-full w-full min-h-screen min-w-screen flex flex-col items-center bg-gray-600 p-6">
+    <div className="h-full w-full min-h-screen min-w-screen flex flex-col items-center p-6">
       <Header />
 
       <Game />
